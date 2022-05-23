e92fde29dbdfbcd72013d0c8522d6037c1f93df6
bokdol11859 <2019147551@yonsei.ac.kr>
2022-05-24 00:16:41 +0900

    articleapp implemented

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+
+    title = models.CharField(max_length=200, null=False)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_on = models.DateField(auto_created=True, null=True)
