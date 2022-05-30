c9edfafef502f2b4fd1b6071fbf541e0a1029c7b
bokdol11859 <2019147551@yonsei.ac.kr>
2022-05-31 01:26:06 +0900

    project page with additional articles implemented, my page fixed

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,12 +7,15 @@ from django.urls import reverse, reverse_lazy
 # Create your views here.
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
+from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
+
 
 @login_required
 def hello_world(request):
@@ -31,18 +34,24 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView):
+
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = "target_user"
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(author=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
@@ -52,6 +61,7 @@ class AccountUpdateView(UpdateView):
     context_object_name = "target_user"
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
